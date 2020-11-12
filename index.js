550c88a7882ae5117f14bc7afb7d8ca9512c8c61
o-naichenko <o.naichenko@gmail.com>
2020-11-12 16:38:47 +0200

    Завдання 3 done
    Був змушений передати об'єкт аргументів в logSuccess, інакше 'time' був 'undefined'.

--- index.js
+++ index.js
@@ -2,14 +2,14 @@
 // Напиши функцію delay(ms), яка повертає проміс, що переходить в стан "resolved" через ms мілісекунд.
 // Значенням промісу, яке виповнилося має бути та кількість мілісекунд, яку передали під час виклику функції delay.
 
-const delay = (ms) => {
-  const promise = new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(ms);
-    }, ms);
-  });
-  return promise;
-};
+// const delay = (ms) => {
+//   const promise = new Promise((resolve) => {
+//     setTimeout(() => {
+//       resolve(ms);
+//     }, ms);
+//   });
+//   return promise;
+// };
 
 //const logger = (time) => console.log(`Resolved after ${time}ms`);
 
@@ -22,26 +22,26 @@ const delay = (ms) => {
 // Перепиши функцію toggleUserState() так, щоб вона не використовувала callback-функцію callback,
 // а приймала всього два параметри allUsers і userName і повертала проміс.
 
-const users = [
-  { name: "Mango", active: true },
-  { name: "Poly", active: false },
-  { name: "Ajax", active: true },
-  { name: "Lux", active: false },
-];
+// const users = [
+//   { name: "Mango", active: true },
+//   { name: "Poly", active: false },
+//   { name: "Ajax", active: true },
+//   { name: "Lux", active: false },
+// ];
 
-const toggleUserState = (allUsers, userName) => {
-  const promise = new Promise((resolve) => {
-    let updatedUsers = [];
-    resolve(
-      (updatedUsers = allUsers.map((user) =>
-        user.name === userName ? { ...user, active: !user.active } : user
-      ))
-    );
-  });
-  return promise;
-};
+// const toggleUserState = (allUsers, userName) => {
+//   const promise = new Promise((resolve) => {
+//     let updatedUsers = [];
+//     resolve(
+//       (updatedUsers = allUsers.map((user) =>
+//         user.name === userName ? { ...user, active: !user.active } : user
+//       ))
+//     );
+//   });
+//   return promise;
+// };
 
-const logger = (updatedUsers) => console.table(updatedUsers);
+// const logger = (updatedUsers) => console.table(updatedUsers);
 
 // /*
 //  * Зараз працює так
@@ -52,38 +52,42 @@ const logger = (updatedUsers) => console.table(updatedUsers);
 // /*
 //  * Повинно працювати так
 //  */
-toggleUserState(users, "Mango").then(logger);
-toggleUserState(users, "Lux").then(logger);
+// toggleUserState(users, "Mango").then(logger);
+// toggleUserState(users, "Lux").then(logger);
 
 // Завдання 3
 // Перепиши функцію makeTransaction() так, щоб вона не використовувала callback-функції onSuccess і onError,
 // а приймала всього один параметр transaction і повертала проміс.
 
-// const randomIntegerFromInterval = (min, max) => {
-//   return Math.floor(Math.random() * (max - min + 1) + min);
-// };
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
 
-// const makeTransaction = (transaction, onSuccess, onError) => {
-//   const delay = randomIntegerFromInterval(200, 500);
+const makeTransaction = (transaction) => {
+  const delay = randomIntegerFromInterval(200, 500);
 
-//   setTimeout(() => {
-//     const canProcess = Math.random() > 0.3;
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const canProcess = Math.random() > 0.3;
 
-//     if (canProcess) {
-//       onSuccess(transaction.id, delay);
-//     } else {
-//       onError(transaction.id);
-//     }
-//   }, delay);
-// };
+      if (canProcess) {
+        resolve({ id: transaction.id, time: delay });
+      } else {
+        reject(transaction.id);
+      }
+    }, delay);
+  });
+  console.log(promise);
+  return promise;
+};
 
-// const logSuccess = (id, time) => {
-//   console.log(`Transaction ${id} processed in ${time}ms`);
-// };
+const logSuccess = ({ id, time }) => {
+  console.log(`Transaction ${id} processed in ${time}ms`);
+};
 
-// const logError = id => {
-//   console.warn(`Error processing transaction ${id}. Please try again later.`);
-// };
+const logError = (id) => {
+  console.warn(`Error processing transaction ${id}. Please try again later.`);
+};
 
 // /*
 //  * Працює так
@@ -95,18 +99,10 @@ toggleUserState(users, "Lux").then(logger);
 // /*
 //  * Повинно працювати так
 //  */
-// makeTransaction({ id: 70, amount: 150 })
-//   .then(logSuccess)
-//   .catch(logError);
+makeTransaction({ id: 70, amount: 150 }).then(logSuccess).catch(logError);
 
-// makeTransaction({ id: 71, amount: 230 })
-//   .then(logSuccess)
-//   .catch(logError);
+makeTransaction({ id: 71, amount: 230 }).then(logSuccess).catch(logError);
 
-// makeTransaction({ id: 72, amount: 75 })
-//   .then(logSuccess)
-//   .catch(logError);
+makeTransaction({ id: 72, amount: 75 }).then(logSuccess).catch(logError);
 
-// makeTransaction({ id: 73, amount: 100 })
-//   .then(logSuccess)
-//   .catch(logError);
+makeTransaction({ id: 73, amount: 100 }).then(logSuccess).catch(logError);
