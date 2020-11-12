186cb8f3b17475c7255aab41662b5e9a55bbeee9
o-naichenko <o.naichenko@gmail.com>
2020-11-12 14:33:33 +0200

    Завдання 2 done

--- index.js
+++ index.js
@@ -11,44 +11,50 @@ const delay = (ms) => {
   return promise;
 };
 
-const logger = (time) => console.log(`Resolved after ${time}ms`);
+//const logger = (time) => console.log(`Resolved after ${time}ms`);
 
 // // Виклич функції для перевірки
-delay(2000).then(logger); // Resolved after 2000ms
-delay(1000).then(logger); // Resolved after 1000ms
-delay(1500).then(logger); // Resolved after 1500ms
+// delay(2000).then(logger); // Resolved after 2000ms
+// delay(1000).then(logger); // Resolved after 1000ms
+// delay(1500).then(logger); // Resolved after 1500ms
+
 // Завдання 2
 // Перепиши функцію toggleUserState() так, щоб вона не використовувала callback-функцію callback,
 // а приймала всього два параметри allUsers і userName і повертала проміс.
 
-// const users = [
-//   { name: 'Mango', active: true },
-//   { name: 'Poly', active: false },
-//   { name: 'Ajax', active: true },
-//   { name: 'Lux', active: false },
-// ];
+const users = [
+  { name: "Mango", active: true },
+  { name: "Poly", active: false },
+  { name: "Ajax", active: true },
+  { name: "Lux", active: false },
+];
 
-// const toggleUserState = (allUsers, userName, callback) => {
-//   const updatedUsers = allUsers.map(user =>
-//     user.name === userName ? { ...user, active: !user.active } : user,
-//   );
+const toggleUserState = (allUsers, userName) => {
+  const promise = new Promise((resolve) => {
+    let updatedUsers = [];
+    resolve(
+      (updatedUsers = allUsers.map((user) =>
+        user.name === userName ? { ...user, active: !user.active } : user
+      ))
+    );
+  });
+  return promise;
+};
 
-//   callback(updatedUsers);
-// };
-
-// const logger = updatedUsers => console.table(updatedUsers);
+const logger = (updatedUsers) => console.table(updatedUsers);
 
 // /*
 //  * Зараз працює так
 //  */
-// toggleUserState(users, 'Mango', logger);
-// toggleUserState(users, 'Lux', logger);
+// toggleUserState(users, "Mango", logger);
+// toggleUserState(users, "Lux", logger);
 
 // /*
 //  * Повинно працювати так
 //  */
-// toggleUserState(users, 'Mango').then(logger);
-// toggleUserState(users, 'Lux').then(logger);
+toggleUserState(users, "Mango").then(logger);
+toggleUserState(users, "Lux").then(logger);
+
 // Завдання 3
 // Перепиши функцію makeTransaction() так, щоб вона не використовувала callback-функції onSuccess і onError,
 // а приймала всього один параметр transaction і повертала проміс.
